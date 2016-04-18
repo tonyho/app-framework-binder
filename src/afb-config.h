3d28e7d00cd8641bf600d47ed0e3e7e57cb5b439
José Bollo <jose.bollo@iot.bzh>
2016-04-18 11:05:50 +0200

    changing the license to apache 2
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

--- src/afb-config.h
+++ src/afb-config.h
@@ -1,23 +1,20 @@
 /*
-   local-def.h -- provide a REST/HTTP interface
-
-   Copyright (C) 2015, Fulup Ar Foll
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-*/
+ * local-def.h -- provide a REST/HTTP interface
+ *
+ * Copyright (C) 2015, Fulup Ar Foll
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef LOCAL_DEF_H
 #define LOCAL_DEF_H
 
